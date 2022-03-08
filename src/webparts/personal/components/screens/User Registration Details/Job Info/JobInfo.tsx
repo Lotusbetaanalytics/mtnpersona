4f392a14d5b84e8ea64695417088e32b66535601
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-03-08 13:26:05 +0100

    job info

--- src/webparts/personal/components/screens/User Registration Details/Job Info/JobInfo.tsx
+++ src/webparts/personal/components/screens/User Registration Details/Job Info/JobInfo.tsx
@@ -6,44 +6,81 @@ type Props = {};
 
 const JobInfo = (props: Props) => {
   return (
-    <div className={styles.screen1__container}>
+    <div className={styles.screen2__container}>
       <Header />
       <form className={styles.job__form}>
-        <div>
-          <label className={styles.job__label} htmlFor="">
-            How many years have you been in the world of work? Both in and
-            outside MTN
-          </label>
-          <select name="" id="">
-            <option value="">1</option>
-            <option value="">2</option>
-          </select>
+        <div
+          style={{
+            display: "grid",
+            width: "100%",
+            height: "60%",
+            gridTemplateColumns: "1fr",
+          }}
+        >
+          <div>
+            <label className={styles.job__label} htmlFor="">
+              How many years have you been in the world of work? Both in and
+              outside MTN
+            </label>
+            <select name="" id="">
+              <option value="">1</option>
+              <option value="">2</option>
+            </select>
+          </div>
+          <div style={{ margin: "10px 0px" }}>
+            <label
+              className={styles.job__label}
+              htmlFor=""
+              style={{ marginBottom: "10px" }}
+            >
+              Where are you on the Y'ello ladder?
+            </label>
+            <div className={styles.input__details}>
+              <input type="radio" name="hello" />
+              <div> Grace</div>
+            </div>
+            <div className={styles.input__details}>
+              <input type="radio" name="hello" />
+              <div> Mirabel</div>
+            </div>
+          </div>
         </div>
-        <div>
+        <div
+          style={{
+            display: "grid",
+            width: "100%",
+            height: "60%",
+            gridTemplateColumns: "1fr",
+          }}
+        >
           <label className={styles.job__label} htmlFor="">
             Where in our Y'elloverse are you?
           </label>
-          <div>
-            <input type="radio" name="" id="hq" value="HQ" />
+          <div className={styles.input__details}>
+            <input type="radio" name="jobinfo" id="hq" value="HQ" />
             <div>
               HQ (MTN plaza, MTN Penthouse, Y'ellodrome Annex and Akin Adesola)
             </div>
           </div>
 
-          <div>
-            <input type="radio" name="" id="hq" value="LSW" />
+          <div className={styles.input__details}>
+            <div>
+              <input type="radio" name="jobinfo" value="LSW" />
+            </div>
             <div>
               LSW (Aromire, Matari, ojota, Opebi/MM2, Allen, Apapa, Switch, VGC,
               Y'ello City, Ibadan), Benin, Abeokuta
             </div>
           </div>
-          <div>
-            <input type="radio" name="" id="hq" value="eastern region" />
-            <label htmlFor="eastern region">Eastern Region</label>
+          <div className={styles.input__details}>
+            <div>
+              <input type="radio" name="jobinfo" value="eastern region" />
+            </div>
+            <div>Eastern Region</div>
           </div>
-          <div>
-            <input type="radio" name="" id="hq" value="nothern region" />
-            <label htmlFor="nothern region">Nothern Region</label>
+          <div className={styles.input__details}>
+            <input type="radio" name="jobinfo" value="nothern region" />
+            <div>Nothern Region</div>
           </div>
         </div>
       </form>
